c22bd6ee1186b938c12fc743b0748c9a5f6578da
jm <67771030+jmnl225@users.noreply.github.com>
2022-08-11 22:34:31 +0900

    Update background.js

--- lesson6_random,img/background.js
+++ lesson6_random,img/background.js
@@ -1,1 +1,12 @@
+const images = [
+  "0.jpeg", "1.jpeg", "2.jpeg", 
+]
+
+const chosenImg = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImg}`;
+
+document.body.appendChild(bgImage);
 
