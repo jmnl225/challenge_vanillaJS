d953f16f0146453083e2cbc0360c5dcdd0da6260
joomin <67771030+jmnl225@users.noreply.github.com>
2022-08-15 01:35:09 +0900

    complete appgit add --all!

--- lesson7_todolist/js/weather.js
+++ lesson7_todolist/js/weather.js
@@ -1,7 +1,19 @@
+ const API_KEY = 'da6c2654d1ed3735a9cc518b8aa940ca'
+ 
  function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat , lng);
+    const log = position.coords.longitude;
+    console.log("You live in", lat , log);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      const name = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      city.innerText = data.name;
+   });
  }
  function onGeoError(){
     alert("Can't find you. No wather for you.")
